8f7a8c45e25bfd478f891795612ba3741a488aa6
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2018-02-13 12:43:45 +0100

    cleanup

--- scripts/parser.js
+++ scripts/parser.js
@@ -4,180 +4,9 @@ var fs = require('fs');
 var cycle = require('./cycle.js');
 var glslify = require('glslify');
 var GLSL = require('glsl-transpiler');
-var Descriptor = require('glsl-transpiler/lib/descriptor');
 
-// wrap literals
-var oldLiteral = GLSL.prototype.transforms.literal;
-GLSL.prototype.transforms.literal = function(node) {
-  var old = oldLiteral(node);
-  var value = old;
-  if (old.type == 'float') {
-    var s = new String('f(' + old + ')');
-    value = Descriptor(s);
-  }
-  return value;
-};
+var source = glslify(process.argv[2]);
 
-// replace dot operator
-// GLSL.prototype.transforms.oldOperator = GLSL.prototype.transforms.operator;
-/*
-var compile;
-GLSL.prototype.transforms.operator = function(node) {
-  //access operators - expand to arrays
-  if (node.data === '.') {
-    var identNode = node.children[0];
-    var ident = this.process(identNode);
-    var type = ident.type;
-    var prop = node.children[1].data;
-
-    //ab.xyz for example
-    if (/^[xyzwstpdrgba]{1,4}$/.test(prop)) {
-      // prop = prop.toUpperCase();
-      var ret = this.unswizzle(node);
-      var ret2 = Descriptor(`${prop}(${ident})`, ret); 
-      console.log("ret", ret2);
-      return ret2;
-    }
-
-    return Descriptor(`${ident}.${prop}`, {
-      type: type
-    });
-  }
-
-  throw Error('Unknown operator ' + node.data);
-
-  return Descriptor(null);
-}*/
-
-GLSL.prototype.oldUnswizzle = GLSL.prototype.unswizzle;
-GLSL.prototype.unswizzle = function(node) {
-  var identNode = node.children[0];
-  var ident = this.process(identNode);
-  var type = ident.type;
-  var prop = node.children[1].data;
-
-  //ab.xyz for example
-  if (/^[xyzwstpdrgba]{1,4}$/.test(prop)) {
-    // prop = prop.toUpperCase();
-    var ret = this.oldUnswizzle(node);
-    var ret2 = Descriptor(`${prop}(${ident})`, ret); 
-    //console.log("ret", ret2);
-    return ret2;
-  }
-
-  return Descriptor(`${ident}.${prop}`, {
-    type: type
-  });
-};
-
-GLSL.prototype.transforms.assign = function(node) {
-  var result = '';
-  var operator = node.data;
-
-  var right = this.process(node.children[1]);
-  if (node.children[0].type === 'identifier') {
-    var left = Descriptor(node.children[0].data, {
-      type: right.type,
-      optimize: false,
-      complexity: 0
-    });
-  }
-  else {
-    var left = this.process(node.children[0]);
-  }
-
-  var target = left;
-  var isSwizzle = node.children[0].type === 'operator' && /^[xyzwstpdrgba]{1,4}$/.test(node.children[0].children[1].data);
-
-  isSwizzle = false;
-
-  //a *= b.x
-  if (!isSwizzle && this.types[right.type].length == 1 && this.types[target.type].length == 1) {
-    return Descriptor(`${target} ${operator} ${right}`, {
-      type: right.type,
-      complexity: target.complexity + 1 + right.complexity
-    });
-  }
-
-  //FIXME: left can be a structure property set a.prop
-
-  //in cases of setting swizzle - we gotta drop left unswizzle to the right
-  if (isSwizzle) {
-    var positions = this.swizzlePositions(node.children[0].children[1].data);
-    var len = this.types[this.process(node.children[0].children[0]).type].length;
-    var ids = Array(len).fill('null');
-
-    for (var i = 0; i < positions.length; i++) {
-      ids[positions[i]] = i;
-    }
-
-    target = Descriptor(node.children[0].children[0].data, {
-      type: right.type,
-      optimize: false
-    });
-
-    //a.wy *= a.zx →
-    //a = [null, 1, null, 0].map(function (idx, i) {
-    //	return idx == null ? gl_position[i] : this[idx];
-    //}, a.wy * a.zx)
-    if (positions.length > 1) {
-      //*=
-      if (operator.length > 1) {
-        var subOperator = operator.slice(0, -1);
-        right = this.processOperation(this.unswizzle(node.children[0]), right, subOperator);
-        right = this.optimizeDescriptor(right);
-      }
-
-      var comps = Array(len);
-      for (var i = 0; i < len; i++) {
-        comps[i] = Descriptor(`${target}[${i}]`, {
-          type: 'float',
-          complexity: 1
-        });
-      }
-      for (var i = 0; i < positions.length; i++) {
-        comps[positions[i]] = right.components[i];
-      }
-
-      right = Descriptor(
-        `[${ids.join(', ')}].map(function (idx, i) { return idx == null ? ${target}[i] : this[idx]; }, ${right})`, {
-          type: right.type,
-          complexity: len*4 + right.complexity,
-          include: right.include,
-          components: comps
-      });
-      right = this.optimizeDescriptor(right);
-
-      return Descriptor(`${target} =@ ${right}`, {
-        type: right.type,
-        optimize: false,
-        include: right.include
-      });
-    }
-    //a.x *= b → a[0] *= b
-    else {
-      return Descriptor(`${target}[${positions[0]}] ${operator} ${right}`, {
-        type: right.type,
-        optimize: false
-      });
-    }
-  }
-
-  //`a *= x` → `a = a * x`
-  else if (operator.length > 1) {
-    var subOperator = operator.slice(0, -1);
-    right = this.processOperation(left, right, subOperator);
-    right = this.optimizeDescriptor(right);
-  }
-
-  //simple assign, =
-  return Descriptor(`${target} =@ ${right}`, {
-    type: right.type,
-    complexity: 1
-  });
-};
-
-var source = glslify('./source.glsl');
 var options = {
   optimize: true
 };
@@ -298,9 +127,10 @@ function processNode(node) {
         data: ";"
       });
     }
+    /*
     if (node.type == "expr") {
       var leftParen = false;
-      for (var i = node.position; i < node.maxPos; i++) {
+      for (var i = node.position; i < node.position + 1; i++) {
         var node = positions[i].node;
         var token = positions[i].token;
         if (node.data.match(/(/) || token.data.match(/(/)) {
@@ -308,12 +138,13 @@ function processNode(node) {
         }
       }
       if (leftParen) {
+        console.log("leftParen");
         insertToken({
           position: node.maxPos,
           data: ")"
         });
       }
-    }
+    }*/
   }
 
   path.pop();
