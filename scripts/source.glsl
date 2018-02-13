ce09af999c3294db5fecdafa0555619442dd8022
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2018-02-13 12:44:21 +0100

    whitespace (parser testing)

--- scripts/source.glsl
+++ scripts/source.glsl
@@ -13,7 +13,7 @@ void main( void ) {
 	vec2 position = ( gl_FragCoord.xy / resolution.xy ) + mouse / 4.0;
 
 	float color = 0.0;
-	color += sin( position.x * cos( time / 15.0 ) * 80.0 ) + cos( position.y * cos( time / 15.0 ) * 10.0 );
+	color += sin(position.x * cos(time / 15.0) * 80.0) + cos(position.y * cos(time / 15.0) * 10.0);
 	color += sin( position.y * sin( time / 10.0 ) * 40.0 ) + cos( position.x * sin( time / 25.0 ) * 40.0 );
 	color += sin( position.x * sin( time / 5.0 ) * 10.0 ) + sin( position.y * sin( time / 35.0 ) * 80.0 );
 	color *= sin( time / 10.0 ) * 0.5;
